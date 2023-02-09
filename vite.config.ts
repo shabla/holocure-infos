c0f76d487f60fd054f7d58836134326fa6b1bc65
Mathieu Collinge <shabla@gmail.com>
2023-02-09 01:17:42 -0500

    test

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
-	base: "/holocure-infos/",
+	// base: "/holocure-infos/",
 	resolve: {
 		alias: {
 			"@": path.resolve(__dirname, "./src"),

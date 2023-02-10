6769570a0fc918454d9e0538feb1e75b73f60bd5
Mathieu Collinge <shabla@gmail.com>
2023-02-09 22:50:47 -0500

    replaced hashrouter with browserrouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { RouterProvider, createHashRouter } from "react-router-dom";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import { routes } from "@/routes";
 
 import "normalize.css";
 
-const router = createHashRouter(routes);
+const router = createBrowserRouter(routes);
 
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
